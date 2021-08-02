8825e85a5701c464c13be0ea4f51d0afc2f6747f
Christophe <cnamoune@gmail.com>
2021-08-02 11:10:40 +0200

    views/Bills 100% covered by test

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -9,7 +9,6 @@ import Firestore from "../app/Firestore";
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
     test("Then bill icon in vertical layout should be highlighted", () => {
-      jest.mock("../app/Firestore");
       Firestore.bills = () => ({ bills, get: jest.fn().mockResolvedValue() });
       Object.defineProperty(window, "localStorage", { value: localStorageMock });
       window.localStorage.setItem(
@@ -18,12 +17,16 @@ describe("Given I am connected as an employee", () => {
           type: "Employee",
         })
       );
+
+      const html = BillsUI({ data: bills });
       const pathname = ROUTES_PATH["Bills"];
       Object.defineProperty(window, "location", { value: { hash: pathname } });
-      document.body.innerHTML = `<div id="root"></div>`;
+      document.body.innerHTML = `<div id='root'>${html}</div>`;
       Router();
+
       expect(screen.getByTestId("icon-window").classList.contains("active-icon")).toBe(true);
     });
+
     test("Then bills should be ordered from earliest to latest", () => {
       const html = BillsUI({ data: bills });
       document.body.innerHTML = html;
@@ -31,6 +34,13 @@ describe("Given I am connected as an employee", () => {
       const antiChrono = (a, b) => (a < b ? 1 : -1);
       const datesSorted = [...dates].sort(antiChrono);
       expect(dates).toEqual(datesSorted);
+    });
+  });
+  describe("When I am on Bills page but back-end send an error message", () => {
+    test("Then, Error page should be rendered", () => {
+      const html = BillsUI({ error: "some error message" });
+      document.body.innerHTML = html;
+      expect(screen.getAllByText("Erreur")).toBeTruthy();
     });
   });
 });
